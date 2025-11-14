8ddb9726ed4a44b4994ff1b487c884f5771b80c4
nssSSH <218080797+nssSSH@users.noreply.github.com>
2025-11-14 11:31:30 +0530

    added host url
    added host url, so now we can use this host url to detect the other urls
    and remove them, so as to only get unique urls.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use url::{Url, Host, Position};
 use error_chain::error_chain;
 use select::document::Document;
 use select::predicate::Name;
@@ -11,19 +12,24 @@ error_chain! {
 
 #[tokio::main]
 
+
+
 async fn main() -> Result<()>{
     println!("Please enter the site you'd like to scrape: ");
     let mut user_inp = String::new();
     io::stdin().read_line(&mut user_inp).expect("Some shit went wrong!");
+    let parsed = Url::parse(user_inp.trim()).expect("Something went wrong...");
+    let base_url = parsed.host_str().expect("Something went wrong whilst parsing the url. Make sure that it is indeed a real, valid url.");
+    println!("{base_url}");
     let res = reqwest::get(user_inp)
         .await?
         .text()
         .await?;
     println!("The links within the sites are as follows: ");
-    Document::from(res.as_str())
+    let domain_list =Document::from(res.as_str())
         .find(Name("a"))
         .filter_map(|n| n.attr("href"))
         .for_each(|x| println!("{x}"));
-
+    
     Ok(())
 }
